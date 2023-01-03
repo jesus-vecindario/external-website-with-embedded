2dc841171738cd5a53f0461e3c92ffcef1b473ab
Jesus Martinez <jesusda91@gmail.com>
2023-01-03 16:19:35 -0500

    update env

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
 export const SCHEDULER_EMBEDDED =
 	process.env.REACT_APP_SCHEDULER_EMBEDDED_URL ||
-	"https://scheduler-client-embedded.s3.us-east-2.amazonaws.com/script-scheduler.js";
+	"https://suite-scheduler-embedded-testing.s3.us-east-2.amazonaws.com/script-scheduler.js";
 export const SLUG = process.env.REACT_APP_SLUG || "ecoh";
